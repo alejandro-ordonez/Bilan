7b914c6a26bb3135394ea8ffe8481b5f9e03f1de
Alejandro Ordoñez <m.alejandro1898@gmail.com>
2021-03-18 21:01:22 -0500

    Project initialized and renewed

---
+++ bilan-backend/src/test/java/org/bilan/co/bilanbackend/BilanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bilan.co.bilanbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilanBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
