10c2893dd6f16b3312c843c49260156167289dc1
lichblitz <acid.storm3@gmail.com>
2021-05-17 20:27:42 -0500

    feat: add tribes test

---
+++ core/src/main/java/org/bilan/co/data/TribesRepository.java
@@ -0,0 +1,8 @@
+package org.bilan.co.data;
+
+import org.bilan.co.domain.entities.Tribes;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TribesRepository extends JpaRepository<Tribes, Integer> {
+
+}
