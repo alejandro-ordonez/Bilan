373bcc007c139de9e6932201c706bdf995a7a944
lichblitz <acid.storm3@gmail.com>
2021-06-17 23:09:05 -0500

    feat: add docker integration for dev

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Backend for Ministerio de Educación Nacional
 Follow contribution guidelines:
 
 https://gist.github.com/lichblitz/2bdffe469171b1a6987fa2af41321a98
+
+Run 'setup.sh' to configure local environment with docker
--- anexo_script_ddl/install.sh
+++ anexo_script_ddl/install.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
-default_anexo_path="./Anexo3.csv" 
-default_sql_path="./bilandb.sql" 
+default_anexo_path="/var/www/html/Anexo3.csv" 
+default_sql_path="/var/www/html/bilandb.sql" 
 
 if [ ! -r "$default_anexo_path" ]; then
     echo "Usually the anenexed file should be in the path /tmp please check this out";
     exit 1;
 fi
 
-read -p "Enter Db user: " username;
-read -s -p "Enter Db password: " password;
+# read -p "Enter Db user: " username;
+# read -s -p "Enter Db password: " password;
 
 echo -e "\nAssinging the annexed path...";
 sed -i "s|[%]\{1\}InFileAnexoPath[%]\{1\}|$default_anexo_path|gI" "$default_sql_path";
 echo "Done!";
 
 echo "Running the sql file..."
-mariadb -u$username -p$password << EOF
+mariadb  << EOF
 source $default_sql_path;
 exit
 commit;
