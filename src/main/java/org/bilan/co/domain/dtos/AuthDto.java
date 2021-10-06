63de4de21c899103c6d55d2bb4af9e44e002aa4c
John Fonseca <jhfonseca72@gmail.com>
2021-10-06 15:48:44 -0500

    ignore grade property in login

--- src/main/java/org/bilan/co/domain/dtos/AuthDto.java
+++ src/main/java/org/bilan/co/domain/dtos/AuthDto.java
@@ -1,12 +1,16 @@
 package org.bilan.co.domain.dtos;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import org.bilan.co.domain.enums.DocumentType;
 import org.bilan.co.domain.enums.UserType;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AuthDto extends AuthenticatedUserDto {
 
     private String password;
