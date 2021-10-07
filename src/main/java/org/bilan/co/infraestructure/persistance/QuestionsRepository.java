e7424d116f66b92e8bd19cee1320e1f8a8afdddf
Alejandro Ordoñez <m.alejandro1898@gmail.com>
2021-10-07 15:00:02 -0500

    Added ramdon questions

--- src/main/java/org/bilan/co/infraestructure/persistance/QuestionsRepository.java
+++ src/main/java/org/bilan/co/infraestructure/persistance/QuestionsRepository.java
@@ -14,12 +14,13 @@ public interface QuestionsRepository extends JpaRepository<Questions, Integer> {
     @Query("SELECT question FROM Questions question " +
             "WHERE question.grade = :grade AND " +
             "question.idTribe.id = :tribeId " +
-            "GROUP BY question.contexts.id")
+            "GROUP BY question.contexts.id " +
+            "ORDER BY RAND()")
     List<List<Questions>> getQuestions(Pageable pageRequest, String grade, Integer tribeId);
 
 
     @Query("SELECT question FROM Questions question " +
             "WHERE question.grade = :grade "+
-            "GROUP BY question.contexts.id")
+            "GROUP BY question.contexts.id ")
     List<List<Questions>> getQuestions(String grade);
 }
