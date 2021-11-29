cba89d7a9ca3d0c027d6e8750cfbc5759269ba2b
Alejandro Ordoñez <m.alejandro1898@gmail.com>
2021-11-28 23:09:04 -0500

    Added filter to don't repeat questions

--- src/main/java/org/bilan/co/infraestructure/persistance/QuestionsRepository.java
+++ src/main/java/org/bilan/co/infraestructure/persistance/QuestionsRepository.java
@@ -11,16 +11,30 @@ import java.util.List;
 @Repository
 public interface QuestionsRepository extends JpaRepository<Questions, Integer> {
 
-    @Query("SELECT question FROM Questions question " +
-            "WHERE question.grade = :grade AND " +
-            "question.idTribe.id = :tribeId " +
-            "GROUP BY question.contexts.id " +
-            "ORDER BY RAND()")
-    List<List<Questions>> getQuestions(Pageable pageRequest, String grade, Integer tribeId);
+    @Query(value = " SELECT question.* FROM questions question\n" +
+            "\tLEFT JOIN (\n" +
+            "\t\tSELECT * FROM (\n" +
+            "\t\t\tSELECT q.id AS qId, SUM(a.is_correct) AS times_correct FROM questions q \n" +
+            "\t\t\tLEFT JOIN resolved_answer_by r ON q.id = r.id_question \n" +
+            "\t\t\tLEFT JOIN answers a ON r.id_answer = a.id \n" +
+            "\t\t\tWHERE r.student_id_document = :document\n" +
+            "\t\t\tGROUP BY q.id) AS questions_completed \n" +
+            "\t\t\tWHERE times_correct >=3\n" +
+            "\t\t) AS results \n" +
+            "\tON qId=question.id WHERE qId IS NULL AND question.id_tribe = :tribeId AND question.grade = :grade\n" +
+            "\tORDER BY RAND() LIMIT :nQuestions\n", nativeQuery = true)
+    List<Questions> getQuestions(Integer nQuestions, String document, String grade, Integer tribeId);
+
+
+    @Query( value = "SELECT id FROM (SELECT q.id, SUM(a.is_correct) AS times_correct FROM questions q \n" +
+            "LEFT JOIN resolved_answer_by r ON q.id = r.id_question \n" +
+            "LEFT JOIN answers a ON r.id_answer = a.id \n" +
+            "WHERE r.student_id_document = '123456789'\n" +
+            "GROUP BY q.id) AS questions_completed WHERE times_correct >=3", nativeQuery = true)
+    List<Integer> getAlreadyResolvedQuestions(String document);
 
 
     @Query("SELECT question FROM Questions question " +
-            "WHERE question.grade = :grade "+
-            "GROUP BY question.contexts.id ")
-    List<List<Questions>> getQuestions(String grade);
+            "WHERE question.grade = :grade ")
+    List<Questions> getQuestions(String grade);
 }
