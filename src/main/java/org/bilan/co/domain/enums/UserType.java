8d8f09a0af9d17a311e86cf382f44182e67f8909
John Fonseca <jhfonseca72@gmail.com>
2021-12-01 10:47:34 -0500

    fix user type endpoint user info

--- src/main/java/org/bilan/co/domain/enums/UserType.java
+++ src/main/java/org/bilan/co/domain/enums/UserType.java
@@ -1,5 +1,25 @@
 package org.bilan.co.domain.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Arrays;
+
+@AllArgsConstructor
+@Getter
 public enum UserType {
-    Teacher, DirectiveUser, Student, Min, Admin, Unknown
+    Teacher("TEACHER"),
+    DirectiveUser("DIRECT_TEACHER"),
+    Student("STUDENT"),
+    Min("SEC_EDU"),
+    Admin("ADMIN"),
+    Unknown("");
+    private final String role;
+
+    public static UserType findByRol(String rol) {
+        return Arrays.stream(UserType.values())
+                .filter(type -> rol.equals(type.getRole()))
+                .findFirst()
+                .orElse(UserType.Unknown);
+    }
 }
