1e47627a28ef3cbc86da5cda9e924723f1b6514e
Alejandro Ordoñez <m.alejandro1898@gmail.com>
2021-12-03 12:59:53 -0500

    Linked time in game

--- src/main/java/org/bilan/co/infraestructure/persistance/StatsRepository.java
+++ src/main/java/org/bilan/co/infraestructure/persistance/StatsRepository.java
@@ -11,4 +11,8 @@ public interface StatsRepository extends JpaRepository<StudentStats, Integer> {
     @Query(value="SELECT stats FROM StudentStats stats " +
             "INNER JOIN stats.idStudent student WHERE student.document = ?1")
     StudentStats findByDocument(String document);
+
+
+    @Query(value = "SELECT s.time_in_game FROM student_stats s WHERE s.id_student = :document", nativeQuery = true)
+    Float getTimeInApp(String document);
 }
