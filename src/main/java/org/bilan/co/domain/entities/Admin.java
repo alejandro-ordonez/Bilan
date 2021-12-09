2a3912384694a00a3bf147584035c527ac05132b
John Fonseca <33574327+jhfonseca72@users.noreply.github.com>
2021-12-09 12:47:45 -0500

    add role for each dashboard (#71)

--- src/main/java/org/bilan/co/domain/entities/Admin.java
+++ src/main/java/org/bilan/co/domain/entities/Admin.java
@@ -1,10 +1,14 @@
 package org.bilan.co.domain.entities;
 
+import org.bilan.co.domain.enums.UserType;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 @Table(name = "admin")
 @Entity
 public class Admin extends UserInfo {
-
+    @Transient
+    private UserType userType = UserType.Admin;
 }
