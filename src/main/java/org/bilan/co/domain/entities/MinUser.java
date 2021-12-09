01096e8fe814d4c96f1ac49076cc5559173d24a0
John Fonseca <jhfonseca72@gmail.com>
2021-12-09 12:45:54 -0500

    add role for each dashboard

--- src/main/java/org/bilan/co/domain/entities/MinUser.java
+++ src/main/java/org/bilan/co/domain/entities/MinUser.java
@@ -1,8 +1,12 @@
 package org.bilan.co.domain.entities;
 
+import org.bilan.co.domain.enums.UserType;
+
 import javax.persistence.Entity;
+import javax.persistence.Transient;
 
 @Entity
 public class MinUser extends UserInfo {
-
+    @Transient
+    private UserType userType = UserType.Min;
 }
