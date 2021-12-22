99f0376bb047f9fc0c228a41d0465995a00f40b4
Alejandro Ordoñez <m.alejandro1898@gmail.com>
2021-12-21 22:51:55 -0500

    Added default admin. It needs to be removed later.

--- src/main/java/org/bilan/co/BilanBackendApplication.java
+++ src/main/java/org/bilan/co/BilanBackendApplication.java
@@ -1,10 +1,17 @@
 package org.bilan.co;
 
 import lombok.extern.slf4j.Slf4j;
+import org.bilan.co.domain.entities.Roles;
+import org.bilan.co.domain.entities.UserInfo;
+import org.bilan.co.domain.enums.DocumentType;
+import org.bilan.co.infraestructure.persistance.UserInfoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Slf4j
@@ -19,5 +26,30 @@ public class BilanBackendApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BilanBackendApplication.class, args);
+	}
+
+	@Autowired
+	private UserInfoRepository userInfoRepository;
+
+	@EventListener
+	public void seedAdmin(ContextRefreshedEvent evet){
+		if(userInfoRepository.existsById("1024506030"))
+			return;
+
+		UserInfo userInfo = new UserInfo();
+		userInfo.setDocument("1024506030");
+		userInfo.setName("David");
+		userInfo.setLastName("Avendaño");
+		userInfo.setPositionName("MEN");
+		userInfo.setPassword("$2a$12$OSEerOKttf9biSyWnTbXYO/juup8Ax2l17ACO4n3A7cEdW5OMP0ai");
+		userInfo.setDocumentType(DocumentType.CC);
+
+		Roles role = new Roles();
+		role.setId(5);
+
+		userInfo.setRole(role);
+		userInfo.setIsEnabled(true);
+
+		userInfoRepository.save(userInfo);
 	}
 }
