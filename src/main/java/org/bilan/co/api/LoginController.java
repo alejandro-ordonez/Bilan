ce9e3668500545f9387c1ca3b80262cca75eb042
Alejandro Ordoñez <m.alejandro.ordonez@outlook.com>
2025-06-18 21:13:15 -0500

    fix: Block any other users from login when game is closing

--- src/main/java/org/bilan/co/api/LoginController.java
+++ src/main/java/org/bilan/co/api/LoginController.java
@@ -1,9 +1,12 @@
 package org.bilan.co.api;
 
 import jakarta.validation.Valid;
+import org.bilan.co.application.game.IGameCycleService;
 import org.bilan.co.application.user.ILoginService;
 import org.bilan.co.domain.dtos.ResponseDto;
 import org.bilan.co.domain.dtos.user.AuthDto;
+import org.bilan.co.domain.enums.GameCycleStatus;
+import org.bilan.co.domain.enums.UserType;
 import org.bilan.co.ws.simat.client.SimatEstudianteClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,17 +17,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth/login")
 public class LoginController {
-
-    private final SimatEstudianteClient simatEstudianteClient;
     private final ILoginService loginService;
+    private final IGameCycleService gameCycleService;
 
-    public LoginController(SimatEstudianteClient simatEstudianteClient, ILoginService loginService) {
-        this.simatEstudianteClient = simatEstudianteClient;
+    public LoginController(ILoginService loginService, IGameCycleService gameCycleService) {
         this.loginService = loginService;
+        this.gameCycleService = gameCycleService;
     }
 
     @PostMapping
     public ResponseEntity<ResponseDto<String>> login(@Valid @RequestBody AuthDto login) {
+        var currentCycle = gameCycleService.getCurrentCycle();
+
+        if(login.getUserType() != UserType.Admin &&
+                currentCycle.getResult().getGameStatus() == GameCycleStatus.ProcessingClosing){
+
+            return ResponseEntity
+                    .status(503)
+                    .build();
+        }
+
+
         return loginService.doLogin(login);
     }
 }
