f602f8acae563835a8862f07620ba0e82914a4f9
Alejandro Ordoñez <m.alejandro.ordonez@outlook.com>
2025-08-08 15:26:03 -0500

    feat: add new document types

--- src/main/java/org/bilan/co/domain/enums/DocumentType.java
+++ src/main/java/org/bilan/co/domain/enums/DocumentType.java
@@ -1,5 +1,42 @@
 package org.bilan.co.domain.enums;
 
 public enum DocumentType {
-    CC, TI, RC, CE, Unknown
+    /**
+     * Cédula de ciudadania
+     */
+    CC,
+
+    /**
+     * Tarjeta de identidad
+     */
+    TI,
+
+    /**
+     * Registro civil
+     */
+    RC,
+
+    /**
+     * Cédula de extranjería
+     */
+    CE,
+
+    /**
+     * Persona expuesta políticamente
+     */
+    PEP,
+
+    /**
+     * Número establecido por la secretaría de educación.
+     */
+    NES,
+
+    /**
+     * Tarjeta de movilidad fronteriza
+     */
+    TMF,
+
+    VISA,
+
+    Unknown
 }
